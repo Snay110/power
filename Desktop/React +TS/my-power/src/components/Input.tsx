63040cee310b1cfeb5261014fad0cd9b637d8a9a
Oleksandr Murga <sanay8myrga@gmail.com>
2025-05-18 15:05:09 +0300

    fix: useState как обьект

--- Desktop/React +TS/my-power/src/components/Input.tsx
+++ Desktop/React +TS/my-power/src/components/Input.tsx
@@ -1,20 +1,34 @@
 import { useState } from "react";
 
 export default function Input() {
-  const [value, setValue] = useState("");
-  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
-    setValue(event.target.value);
-  }
+  const [formData, setFormData] = useState({
+    name: "",
+    LastName: "",
+    email: "",
+  });
+
   return (
     <section>
       <label htmlFor="">Name</label>
-      <input className="" type={"text"} value={value} onChange={handleChange} />
+      <input
+        type={"text"}
+        value={formData.name}
+        onChange={(e) => setFormData({ ...formData, name: e.target.value })}
+      />
 
       <label htmlFor="">LastName</label>
-      <input type={"text"} value={value} onChange={handleChange} />
+      <input
+        type={"text"}
+        value={formData.LastName}
+        onChange={(e) => setFormData({ ...formData, LastName: e.target.value })}
+      />
 
       <label htmlFor="">email</label>
-      <input type={"text"} value={value} onChange={handleChange} />
+      <input
+        type={"text"}
+        value={formData.email}
+        onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+      />
     </section>
   );
 }
