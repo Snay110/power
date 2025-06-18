89effbbe6fcd40519335942c81f1cca6e773143f
Oleksandr Murga <sanay8myrga@gmail.com>
2025-06-19 01:41:10 +0300

    feat: вынес асинхронную логику добавления привычки в кастомный хук useAddHabit с использованием useCallback

--- Desktop/React +TS/my-power/src/components/fristInput.tsx
+++ Desktop/React +TS/my-power/src/components/fristInput.tsx
@@ -1,33 +1,42 @@
 import { useState, useEffect } from "react";
 import { Ways } from "../data";
 import "../Button/Button.css";
-import { handleSubmit } from "./handleSubmit";
 import HabitForm from "./habitForm";
 import { ListItems } from "./listItems";
 import { UserHabits } from "./userHabits";
 import { handleDelete } from "./handleDelete";
 import { onDelete } from "./onDelete";
+import { useAddHabit } from "./habitsAsync";
+
 export default function EffectHandle() {
   const [habitInput, setHabitInput] = useState("");
   const [habits, setHabits] = useState<{ id: string; label: string }[]>([]);
   const [ways, setWays] = useState(Ways);
 
+  const { addHabit } = useAddHabit({ habitInput, setHabitInput, setHabits });
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (habitInput.trim() === "") return;
+
+    await addHabit(habitInput); 
+  };
+
   useEffect(() => {
     const habitStore = localStorage.getItem("habits");
     if (habitStore) {
-      const parse = JSON.parse(habitStore)
+      const parse = JSON.parse(habitStore);
       setHabits(parse);
     }
   }, []);
+
   return (
     <section>
       <HabitForm
         habitInput={habitInput}
         setHabitInput={setHabitInput}
         setHabits={setHabits}
-        handleSubmit={(e) =>
-          handleSubmit({ e, habitInput, setHabitInput, setHabits })
-        }
+        handleSubmit={handleSubmit}
       />
       <ul>
         <ListItems ways={ways} onDelete={onDelete} setWays={setWays} />
--- Desktop/React +TS/my-power/src/components/habitsAsync.tsx
+++ Desktop/React +TS/my-power/src/components/habitsAsync.tsx
@@ -1,34 +1,39 @@
 import type { Async } from "./types";
-
+import { useCallback } from "react";
 import { toast } from "react-toastify";
-export async function addHabitAsync({
+export  function useAddHabit({
   setHabitInput,
   setHabits,
-  habitInput,
-}: Async): Promise<void> {
-  if (habitInput.trim() === "") {
-    return;
-  }
-  const newHabit = {
-    id: crypto.randomUUID(),
-    label: habitInput.trim(),
-  };
-  try {
-    const result = await new Promise<typeof newHabit>((resolve) => {
-      //typeof newHabit is needed to get permission with the same type
-      setTimeout(() => resolve(newHabit), 500);
-    });
+}: Async) {
+  const addHabit = useCallback(
+    async (habitInput: string) => {
+      if (habitInput.trim() === "") {
+        return;
+      }
+      const newHabit = {
+        id: crypto.randomUUID(),
+        label: habitInput.trim(),
+      };
+      try {
+        const result = await new Promise<typeof newHabit>((resolve) => {
+          //typeof newHabit is needed to get permission with the same type
+          setTimeout(() => resolve(newHabit), 500);
+        });
 
-    setHabits((prev) => [...prev, result]);
+        setHabits((prev) => [...prev, result]);
 
-    const habitStore = JSON.parse(localStorage.getItem("habits") || "[]");
+        const habitStore = JSON.parse(localStorage.getItem("habits") || "[]");
 
-    localStorage.setItem("habits", JSON.stringify([...habitStore, result]));
+        localStorage.setItem("habits", JSON.stringify([...habitStore, result]));
 
-    toast.success("Habit successfully added");
-  } catch {
-    toast.error("error adding habit.");
-  } finally {
-    setHabitInput("");
-  }
+        toast.success("Habit successfully added");
+      } catch {
+        toast.error("error adding habit.");
+      } finally {
+        setHabitInput("");
+      }
+    },
+    [setHabits, setHabitInput]
+  );
+  return { addHabit };
 }
