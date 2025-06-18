cba16355651e5bf91f84035d8d6b7f5c019eaf8c
Oleksandr Murga <sanay8myrga@gmail.com>
2025-06-18 15:33:47 +0300

    feat: Добавлено удаление из localStorage

--- Desktop/React +TS/my-power/src/components/handleDelete.tsx
+++ Desktop/React +TS/my-power/src/components/handleDelete.tsx
@@ -1,6 +1,11 @@
-export 
-  function handleDelete(id: string,setHabits:React.Dispatch<
-      React.SetStateAction<{ id: string; label: string }[]>
-    >) {
-    setHabits((prev) => prev.filter((habit) => habit.id !== id));
-  }
+
+export function handleDelete(
+  id: string,
+  setHabits: React.Dispatch<React.SetStateAction<{ id: string; label: string }[]>>
+) {
+  setHabits((prev) => {
+    const updated = prev.filter((habit) => habit.id !== id);
+    localStorage.setItem("habits", JSON.stringify(updated));
+    return updated;
+  });
+}
